04648ca9305b865576123fa9fe79cab6856d6b95
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2011-01-16 19:05:09 +0000

    déplacement de l'évènement Save en tant qu'évènement de controle de l'appli

--- src/fr/prunetwork/atelierkanban/event/control/Save.java
+++ src/fr/prunetwork/atelierkanban/event/control/Save.java
@@ -14,8 +14,9 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package fr.prunetwork.atelierkanban.event;
+package fr.prunetwork.atelierkanban.event.control;
 
+import fr.prunetwork.atelierkanban.event.AbstractEvent;
 import fr.prunetwork.atelierkanban.storage.EventSaver;
 
 /**
