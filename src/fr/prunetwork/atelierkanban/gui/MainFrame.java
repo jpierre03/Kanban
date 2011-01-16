34a56fec9d3d8de516e8417d455bd8f94219d460
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2011-01-16 19:04:38 +0000

    ce qu'il faut pour enregistrer dans un fichier sans formattage des données

--- src/fr/prunetwork/atelierkanban/gui/MainFrame.java
+++ src/fr/prunetwork/atelierkanban/gui/MainFrame.java
@@ -1,21 +1,37 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * MainFrame.java
+ *  Copyright (C) 2010 Team-W@R (team-war@prunetwork.fr)
  *
- * Created on 9 janv. 2011, 05:32:54
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package fr.prunetwork.atelierkanban.gui;
+
+import fr.prunetwork.atelierkanban.storage.writer.StoreDataToFile;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileFilter;
 
 /**
  *
- * @author jpierre03
+ * @author Jean-Pierre Prunaret (jpierre03+twar@prunetwork.fr)
+ * @author garciaf
  */
 public class MainFrame extends javax.swing.JFrame {
+
+    StoreDataToFile storeDataToFile = new StoreDataToFile();
 
     /** Creates new form MainFrame */
     public MainFrame() {
@@ -34,36 +50,88 @@ public class MainFrame extends javax.swing.JFrame {
 
         kanbanPlanningPanel1 = new fr.prunetwork.atelierkanban.gui.KanbanPlanningPanel();
         chronometerPanel1 = new fr.prunetwork.atelierkanban.gui.ChronometerPanel();
+        controlPanel = new fr.prunetwork.atelierkanban.gui.ControlPanel();
+        menuBar = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        saveMenuItem = new javax.swing.JMenuItem();
+        editMenu = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new java.awt.GridBagLayout());
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.FIRST_LINE_START;
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         getContentPane().add(kanbanPlanningPanel1, gridBagConstraints);
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridy = 1;
         gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
         getContentPane().add(chronometerPanel1, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 3;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        getContentPane().add(controlPanel, gridBagConstraints);
+
+        fileMenu.setText("File");
+
+        saveMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
+        saveMenuItem.setText("Save");
+        saveMenuItem.setToolTipText("Save into a file");
+        saveMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saveMenuItemActionPerformed(evt);
+            }
+        });
+        fileMenu.add(saveMenuItem);
+
+        menuBar.add(fileMenu);
+
+        editMenu.setText("Edit");
+        menuBar.add(editMenu);
+
+        setJMenuBar(menuBar);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void saveMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saveMenuItemActionPerformed
+        JFileChooser chooser = new JFileChooser();
+        // Note: source for ExampleFileFilter can be found in FileChooserDemo,
+        // under the demo/jfc directory in the Java 2 SDK, Standard Edition.
+        int returnVal = chooser.showOpenDialog(this);
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            System.out.println("You chose to open this file: "
+                    + chooser.getSelectedFile().getName());
+            try {
+                storeDataToFile.commit(chooser.getSelectedFile().getAbsolutePath());
+            } catch (IOException ex) {
+                Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }//GEN-LAST:event_saveMenuItemActionPerformed
+
     /**
-    * @param args the command line arguments
-    */
+     * @param args the command line arguments
+     */
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
+
             public void run() {
                 new MainFrame().setVisible(true);
             }
         });
     }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private fr.prunetwork.atelierkanban.gui.ChronometerPanel chronometerPanel1;
+    private fr.prunetwork.atelierkanban.gui.ControlPanel controlPanel;
+    private javax.swing.JMenu editMenu;
+    private javax.swing.JMenu fileMenu;
     private fr.prunetwork.atelierkanban.gui.KanbanPlanningPanel kanbanPlanningPanel1;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JMenuItem saveMenuItem;
     // End of variables declaration//GEN-END:variables
-
 }
---
+++ src/fr/prunetwork/atelierkanban/storage/writer/StoreDataToFile.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (C) 2010 Team-W@R (team-war@prunetwork.fr)
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.prunetwork.atelierkanban.storage.writer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Jean-Pierre Prunaret (jpierre03+twar@prunetwork.fr)
+ * @author garciaf
+ */
+public class StoreDataToFile {
+
+    StringBuilder sb = new StringBuilder();
+
+    public StoreDataToFile() {
+    }
+
+    public void add(String chaine) {
+        sb.append(chaine);
+    }
+
+    public String getBuffer() {
+        return sb.toString();
+    }
+
+    @Override
+    public String toString() {
+        return sb.toString();
+    }
+
+    public void commit(String file) throws IOException {
+        //création ou ajout dans le file texte
+        FileWriter fw = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter outputFilePrintWriter = new PrintWriter(bw);
+        outputFilePrintWriter.println(sb.toString());
+        outputFilePrintWriter.close();
+        System.out.println(file + " has been created !");
+    }
+}
