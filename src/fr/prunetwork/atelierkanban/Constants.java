1469b8c944ab071a69595b72cbcb90f5f0491168
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2011-01-16 19:04:16 +0000

    ajout de la licence GPL3

--- src/fr/prunetwork/atelierkanban/Constants.java
+++ src/fr/prunetwork/atelierkanban/Constants.java
@@ -1,14 +1,25 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright (C) 2010 Jean-Pierre Prunaret (jpierre03+kanban@prunetwork.fr)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package fr.prunetwork.atelierkanban;
 
 
 /**
  *
- * @author jpierre03
+ * @author  Jean-Pierre Prunaret (jpierre03+kanban@prunetwork.fr)
  */
 public class Constants {
 	public static final String SPLASH_SCREEN_IMAGE="/images/splash-1024x800.png";
--- src/fr/prunetwork/atelierkanban/gui/ChronometerPanel.java
+++ src/fr/prunetwork/atelierkanban/gui/ChronometerPanel.java
@@ -1,26 +1,33 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * ChronometerPanel.java
+ *  Copyright (C) 2010 Jean-Pierre Prunaret (jpierre03+kanban@prunetwork.fr)
  *
- * Created on 7 janv. 2011, 16:11:16
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package fr.prunetwork.atelierkanban.gui;
 
-import fr.prunetwork.atelierkanban.Chronometer;
 import java.awt.EventQueue;
 import java.util.Timer;
 import java.util.TimerTask;
+
 import javax.swing.JFrame;
-import javax.swing.JWindow;
 import javax.swing.UIManager;
+
+import fr.prunetwork.atelierkanban.Chronometer;
 
 /**
  *
- * @author jpierre03
+ * @author  Jean-Pierre Prunaret (jpierre03+kanban@prunetwork.fr)
  */
 public class ChronometerPanel extends javax.swing.JPanel {
 
