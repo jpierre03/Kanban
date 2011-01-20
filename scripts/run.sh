64bf178943a00a3293e9e1103b423f429b95e023
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2011-01-20 00:30:23 +0000

    run bourrin de test (unix)

---
+++ scripts/run.sh
@@ -0,0 +1,8 @@
+java -cp AtelierKanban.jar fr.prunetwork.network.server.MyServerMultiClientTest	&
+java -cp AtelierKanban.jar fr.prunetwork.atelierkanban.MainObserver		&
+java -cp AtelierKanban.jar fr.prunetwork.atelierkanban.WorkStation1		&
+java -cp AtelierKanban.jar fr.prunetwork.atelierkanban.WorkStation2		&
+java -cp AtelierKanban.jar fr.prunetwork.atelierkanban.WorkStation3		&
+java -cp AtelierKanban.jar fr.prunetwork.atelierkanban.WorkStation4		&
+
+
