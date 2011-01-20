92fc8647506b68dfdf78dee502d6d68a5d54cb81
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2011-01-20 00:05:46 +0000

    interfaces de lancement de 6 postes de travail + profils lancement
      * création des différentes interfaces pour les 6 postes de travail
      * créations des profils netbeans associés

---
+++ src/fr/prunetwork/atelierkanban/WorkStation4.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (C) 2010 Jean-Pierre Prunaret (jpierre03+AtelierKanban@prunetwork.fr)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.prunetwork.atelierkanban;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import fr.prunetwork.atelierkanban.gui.MainFrame;
+import fr.prunetwork.atelierkanban.gui.SplashScreenWindows;
+import fr.prunetwork.atelierkanban.storage.EventStore;
+import fr.prunetwork.network.NetworkEventStore;
+
+/**
+ * Start the Application
+ * @author Jean-Pierre Prunaret (jpierre03+AtelierKanban@prunetwork.fr)
+ */
+public class WorkStation4 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception ex) {
+		}
+
+		SplashScreenWindows splashScreen = new SplashScreenWindows();
+
+		MainFrame frame = new MainFrame();
+		frame.setProductNames("4K-3L", "NONE");
+		EventStore es = new EventStore();
+		NetworkEventStore nes = new NetworkEventStore(Constants.DEFAULT_HOSTNAME, Constants.DEFAULT_PORT_NUMBER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.pack();
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException ex) {
+		}
+
+		splashScreen.masquer();
+		frame.setLocationRelativeTo(frame.getParent());
+		frame.setVisible(true);
+	}
+
+	private WorkStation4() {
+	}
+}
