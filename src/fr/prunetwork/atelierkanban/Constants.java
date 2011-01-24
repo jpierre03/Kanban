6bae7b0b03b73402312ae7400e7c64ca0a66433b
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2011-01-24 22:33:20 +0000

    modifications paramètres d'exécution

--- src/fr/prunetwork/atelierkanban/Constants.java
+++ src/fr/prunetwork/atelierkanban/Constants.java
@@ -26,10 +26,10 @@ public class Constants {
 			"/images/splash-1024x800.png";
 	public static final String SHORT_FILE = "/home/jpierre03/Bureau/test";
 	public static final String DEFAULT = "default";
-	public static final int MILISECONDS_BETWEEN_LOADED_EVENTS = 0;
+	public static final int MILISECONDS_BETWEEN_LOADED_EVENTS = 100;
 	public static final int DEFAULT_PORT_NUMBER = 2222;
-//	public static final String DEFAULT_HOSTNAME = "localhost";
-	public static final String DEFAULT_HOSTNAME = "10.0.0.161";
+	public static final String DEFAULT_HOSTNAME = "localhost";
+//	public static final String DEFAULT_HOSTNAME = "10.0.0.161";
 	private Constants() {
 	}
 }
