1e0bda5c0b41244489d3670e69043baccbf3561f
Jean-Pierre PRUNARET <jean-pierre+git@prunetwork.fr>
2012-10-30 00:54:44 +0100

    update copyright to GNU GPL 3

--- src/fr/prunetwork/atelierkanban/gui/ObservatorFrame.java
+++ src/fr/prunetwork/atelierkanban/gui/ObservatorFrame.java
@@ -1,27 +1,27 @@
 /*
- *  Copyright (C) 2010 Jean-Pierre Prunaret (jpierre03+AtelierKanban@prunetwork.fr)
+ * Copyright (C) 2010-2012 Jean-Pierre PRUNARET
  *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * Free Software Foundation, either version 3 of the License, or
+ * any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package fr.prunetwork.atelierkanban.gui;
 
 import fr.prunetwork.atelierkanban.Constants;
+import fr.prunetwork.atelierkanban.event.Event;
 import fr.prunetwork.atelierkanban.storage.reader.ExtractEventFromFile;
 import fr.prunetwork.atelierkanban.storage.writer.StoreDataToFile;
 
 import javax.swing.JFileChooser;
-import java.awt.Event;
 import java.awt.EventQueue;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
